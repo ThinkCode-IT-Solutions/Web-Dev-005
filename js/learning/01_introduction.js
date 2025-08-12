13be2ef685f017bce058ca170a70b299d547ccc4
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-08-12 10:41:11 +0500

    variable done

---
+++ js/learning/01_introduction.js
@@ -0,0 +1,11 @@
+console.log("hello world from the ThinkCode");
+/**
+ * almost every language have these in common 
+    * variables
+    * operators
+    * if/else switch
+    * loop
+    * functions
+    * OOP
+ */
+
---
+++ js/learning/02_variables.js
@@ -0,0 +1,38 @@
+console.log("Variable in js");
+/**
+ * VARIABLES
+ * variable are tiny boxes in the memory (RAM) used to store program data
+ * is equel to is use to store data form right side into the left
+ * let/const is use to start a variable
+ * js is a dynamic typed language
+ * mutability in let and const
+ * datatypes are the type of data stored in a variable
+ */
+
+
+
+const nombre = "Shahzad Farooq"; //string
+let age = 22 // number
+let isMarried = false; // boolean
+let isMuslim  = true;
+
+
+console.log("age is ",age);
+
+console.log("is he married: ",isMarried);
+
+age = 23;
+age = age + 1;
+// nombre = "Ali Imran";
+console.log("age is " , age)
+
+
+
+
+// 4 cases of typing names in programming
+/**
+ * Pascel Case =>  ShahzadFarooq
+ * camel Case =>   shahzadFarooq
+ * snake_case =>   shahzad_farooq
+ * kebab-case =>   shahzad-farooq
+ */
