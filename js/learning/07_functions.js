d9b61024846e0565b109e4554257f2c6b673adc6
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-08-27 00:01:10 +0500

    practice 1

--- js/learning/07_functions.js
+++ js/learning/07_functions.js
@@ -6,7 +6,7 @@ console.log("Welcome to the Functions class");
  *  - function definition
  *  - function call
  * function perameters and function arguments
- * default perameters in function 
+ * default perameters in function
  * functions are of two types in js
  *  - common js functions
  *  - arrow functions
@@ -23,7 +23,7 @@ function sum(num1, num2) {
   console.log(`The sum of ${num1} and ${num2} is ${result} `);
 }
 
-function getPercentage(name , obtained, total=1200) {
+function getPercentage(name, obtained, total = 1200) {
   if (total < obtained) {
     console.log("invalid values");
   } else {
@@ -42,11 +42,42 @@ function getPercentage(name , obtained, total=1200) {
 //     index++;
 // }
 
-sum(12, 35);
-
 let number1 = 34;
 let number2 = 98;
-sum(number1, number2);
 
-getPercentage("shahzad" , 1001, 1100);
-getPercentage("Ayman", 960,1100);
+// let tableOf = prompt("Enter a number to print a table");
+// tableOf = parseInt(tableOf);
+
+// function definition
+function getTable(tableof) {
+  for (let index = 1; index <= 10; index++) {
+    let result = tableof * index;
+    console.log(`${tableof} X ${index} = ${result}`);
+  }
+}
+
+// wasif -> calculator
+let num1 = parseInt(prompt("Enter number 1"));
+let num2 = parseInt(prompt("Enter number 2"));
+let operation = prompt(
+  "Choose an operation \n[1] : Addition (+)\n[2] : Multiplication (*)\n[3] : Division (/)\n[4] : Subtraction (-)\n"
+);
+calculator(num1,num2,operation)
+
+function calculator(n1, n2, ops) {
+  if (ops == 1) {
+    let result = n1 + n2;
+    console.log("the sum is ", result);
+  } else if (ops == 2) {
+    result = n1 * n2;
+    console.log("the multiplication is ", result);
+  } else if (ops == 3) {
+    result = n1 / n2;
+    console.log("the divison is ", result);
+  } else if (ops == 4) {
+    result = n1 - n2;
+    console.log("the minus is ", result);
+  }else{
+    console.log("invalid vale");
+  }
+}
